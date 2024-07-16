18065bb4fe049aef0f9eb5f034617b4c3280706e
ashleeeeeq <ashleeeeeq>
2024-07-16 14:51:18 +0800

    Updated Contact Us Info, scrollbar, added back to top button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('backToTop');
 
+    window.onscroll = function() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    };
+
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({ 
+            top: 0, 
+            behavior: 'smooth' 
+        });
+    });
+});  
