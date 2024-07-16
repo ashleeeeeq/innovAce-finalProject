e4e7ae9bab94d674cf765ee35d862e0c5c3cdd7e
ashleeeeeq <ashleeeeeq>
2024-07-16 19:50:07 +0800

    Removed backToTopButton and Updates JS for smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const backToTopButton = document.getElementById('backToTop');
-
-    window.onscroll = function() {
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            backToTopButton.style.display = 'block';
-        } else {
-            backToTopButton.style.display = 'none';
-        }
-    };
-
-    backToTopButton.addEventListener('click', function() {
-        window.scrollTo({ 
-            top: 0, 
-            behavior: 'smooth' 
-        });
-    });
-}); 
+document.documentElement.style.scrollBehavior = "smooth";
